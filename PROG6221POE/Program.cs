d55be12e0adb9d8f70fc6c8949d9e2ab634d6549
William Mcpetrie <ST10263164@vcconnect.edu.za>
2024-04-16 16:35:59 +0200

    made and copleted createrecipe and made view recipe

--- PROG6221POE/Program.cs
+++ PROG6221POE/Program.cs
@@ -11,59 +11,71 @@ namespace PROG6221POE
 {
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            string option = " ";
-            
-            Console.WriteLine("Welcome to the Cooking Helper!"); //menu
-            Console.WriteLine("Please select an option below");
-            Console.WriteLine("1) Create a new recipe");
-            Console.WriteLine("2) View Recipe");
-            Console.WriteLine("3) ");
-            Console.WriteLine("4) EXIT");
+            bool bMenu = true;
+            string recipeName;
+            while (bMenu == true) //having while loop allows menu to redisplay when finsihed with a specific task
+            {
+                string option = Program.DisplayMenu();
 
+                if (option == "1")
+                {
+                    Console.WriteLine("Please enter the of this recipe");
+                    recipeName = Console.ReadLine();
+
+                    Console.WriteLine("Please enter the number of ingredients in this recipe");
+                    int arraySize = int.Parse(Console.ReadLine());
+                    string[] ingredients = new string[arraySize];
+                    int[] amounts = new int[arraySize];
+                    string[] measurements = new string[arraySize];
+
+                    Console.WriteLine("Please enter the number of steps in this recipe");
+                    int arrayStepsSize = int.Parse(Console.ReadLine());
+                    string[] steps = new string[arrayStepsSize];
+
+                    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+
+                    Recipe recipe = new Recipe();
+                    recipe.CreateRecipe(ingredients, amounts, measurements, steps);
+
+                    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+                }
+                else if (option == "2")
+                {
+                    
+                }
+
+            }  
+        }
+
+        public static string DisplayMenu() //menu
+        {
+            string option = "";
+            bool validOption= false;
+
+            Console.WriteLine("Welcome to the Cooking Helper!\nPlease select an option below:\n1) Create a new recipe\n2) View Recipe\n3)\n4) EXIT");
+            Console.WriteLine("-------------------------------------------------------------------------------------------------------");
             option = Console.ReadLine();
 
-            if (option == "1") 
+            while (validOption == false) 
             {
-
+                if (option == "1" || option == "2" || option == "3")
+                {
+                    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+                    validOption = true;
+                }
+                else if (option == "4")
+                {
+                    System.Environment.Exit(0); //https://www.c-sharpcorner.com/UploadFile/c713c3/how-to-exit-in-C-Sharp/
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input... Please enter either 1,2,3 or 4");
+                    Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+                }
             }
-            else if (option == "2") 
-            {
-
-            }
-            else if (option == "3") 
-            {
-
-            }
-            else if(option == "4") 
-            {
-                System.Environment.Exit(0); //https://www.c-sharpcorner.com/UploadFile/c713c3/how-to-exit-in-C-Sharp/
-            }
-            else 
-            {
-
-            }
-
- 
-        }
-
-        private string CreateRecipe()
-        {
-            string recipe = "";
-            string ingredient = "";
-            double amount = 0;
-
-
-
-            return "Recipe recorded!"; 
-        }
-
-        private string ViewRecipe()
-        {
-            string recipe = "";
-
-            return recipe;
+            return option;
         }
 
     }
---
+++ PROG6221POE/Recipe.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221POE
+{
+    internal class Recipe
+    {
+        public string CreateRecipe(string[] ingredients, int[] amounts, string[] measurements, string[] steps) 
+        {
+            string input = "";
+
+            for (int i = 0; i < ingredients.Length; i++) 
+            {
+                Console.WriteLine("Please enter the name of ingredient " + (i+1));
+                ingredients[i] = Console.ReadLine();
+
+
+                Console.WriteLine("Please enter the quantity of that ingredient");
+                amounts[i] = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please choose a measurement type\n1) Millileters\n2) Teaspoons\n3) Tablespoons\n4) Cups\n5) Whole(for whole items like eggs)");
+                input = Console.ReadLine();
+                if (input == "1")
+                {
+                    measurements[i] = "Millileters";
+                }
+                else if (input == "2")
+                {
+                    measurements[i] = "Teaspoons";
+                }
+                else if (input == "3")
+                {
+                    measurements[i] = "Tablespoons";
+                }
+                else if (input == "4")
+                {
+                    measurements[i] = "Cups";
+                }
+                else if (input == "5")
+                {
+                    measurements[i] = "Whole";
+                }
+                else 
+                {
+                    //invalid input
+                }
+
+                Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+            }
+
+            for (int i = 0; i < steps.Length; i++) 
+            {
+                Console.WriteLine("Please type out step number " + (i+1));
+                steps[i] = Console.ReadLine();
+
+                Console.WriteLine("-------------------------------------------------------------------------------------------------------");
+            }
+
+
+                return "Well done! Recipe successfully created!\n-------------------------------------------------------------------------------------------------------";
+        }
+
+        public string ViewRecipe(string recipeName, string[] ingredients, int[] amounts, string[] measurements, string[] steps) 
+        {
+            string recipe;
+            recipe = recipeName;
+            recipe = recipe + "\n\nINGREDIENTS";
+            for (int i = 0; i < ingredients.Length; i++) 
+            {
+                
+            }
+
+                return recipe;
+        }
+
+
+    }
+}
